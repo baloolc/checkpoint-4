191ee91c3bae6d306e996951d0c6295466e9cedf
baloolc <viann45@hotmail.fr>
2022-07-21 12:39:08 +0200

    granted collection

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -7,11 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 
 class CardController extends AbstractController
 {
     #[Route('/collection', name: 'card')]
+    #[IsGranted('ROLE_USER')]
     public function index(CallApiService $callApiService): Response
     {
         /** @var User  */
